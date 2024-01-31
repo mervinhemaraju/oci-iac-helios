6d0211293dcfe8b32bf9d0cd6b4d08b186443bbb
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-01-31 20:42:16 +0400

    -

--- networking.tf
+++ networking.tf
@@ -1,13 +1,13 @@
 # * Create a virtual cloud network for OCI
-resource "oci_core_vcn" "web" {
+# resource "oci_core_vcn" "web" {
 
-  compartment_id = local.constants.compartment_id_production
+#   compartment_id = local.constants.compartment_id_production
 
-  cidr_blocks    = ["10.16.0.0/16"]
-  display_name   = "web-vcn"
-  dns_label      = "web"
-  is_ipv6enabled = false
-}
+#   cidr_blocks    = ["10.16.0.0/16"]
+#   display_name   = "web-vcn"
+#   dns_label      = "web"
+#   is_ipv6enabled = false
+# }
 
 # * Create a public subnet for web
 # resource "oci_core_subnet" "subnet-public-web" {
