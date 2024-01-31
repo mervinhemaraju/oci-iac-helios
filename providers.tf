b50567d1832695cef70f35169daf74983ee63bb2
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-01-31 16:53:51 +0400

    - Added OCI budget

--- providers.tf
+++ providers.tf
@@ -1,3 +1,9 @@
+
+# * Infisical Provider for Infisical Secrets Manager
+provider "infisical" {
+  service_token = var.token_infisical_iac_cloud_main
+}
+
 # * OCI Provider for Oracle cloud connection
 provider "oci" {
   tenancy_ocid = data.infisical_secrets.cloud_iac_main.secrets["OCI_HELIOS_TENANCY_OCID"].value
@@ -7,23 +13,18 @@ provider "oci" {
   region       = "af-johannesburg-1"
 }
 
-# * Infisical Provider for Infisical Secrets Manager
-provider "infisical" {
-  service_token = var.token_infisical_iac_cloud_main
-}
-
 # * The Terraform Module
 terraform {
 
-  # * AWS Provider
+  # * Required providers
   required_providers {
-
-    oci = {
-      source = "oracle/oci"
-    }
 
     infisical = {
       source = "infisical/infisical"
+    }
+
+    oci = {
+      source = "oracle/oci"
     }
   }
 }
