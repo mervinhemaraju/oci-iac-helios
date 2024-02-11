7b11929b4ec743512c606b4ed3613a2d42eac940
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-02-11 18:48:01 +0400

    - Added new compute web-02 - Added data source for private address for web-02 - Added public ip reservation for web-02

--- compute.tf
+++ compute.tf
@@ -1,6 +1,7 @@
 
 # > OCI Compute Instance Resources
-# * Create a compute instance for web
+
+# * Create a compute instance for web 01
 resource "oci_core_instance" "web_01" {
 
   availability_domain = data.oci_identity_availability_domain.availability_domain.name
@@ -13,7 +14,7 @@ resource "oci_core_instance" "web_01" {
   create_vnic_details {
     subnet_id        = oci_core_subnet.web_public.id
     assign_public_ip = false
-    private_ip       = "10.16.1.223"
+    private_ip       = local.constants.compute.ip_address.web_01
   }
 
   source_details {
@@ -28,16 +29,30 @@ resource "oci_core_instance" "web_01" {
 
 }
 
-# * Create a reserved public ip
-resource "oci_core_public_ip" "web_01" {
+# * Create a compute instance for web 02
+resource "oci_core_instance" "web_02" {
 
-  compartment_id = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
-  lifetime       = "RESERVED"
+  availability_domain = data.oci_identity_availability_domain.availability_domain.name
+  compartment_id      = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
 
-  display_name  = "res-ip-web-01"
-  private_ip_id = data.oci_core_private_ips.web_01.private_ips[0]["id"]
+  display_name = "web-02"
 
-  depends_on = [
-    oci_core_subnet.web_public
-  ]
+  shape = "VM.Standard.E2.1.Micro"
+
+  create_vnic_details {
+    subnet_id        = oci_core_subnet.web_public.id
+    assign_public_ip = false
+    private_ip       = local.constants.compute.ip_address.web_02
+  }
+
+  source_details {
+    source_type             = "image"
+    source_id               = local.constants.compute.image.ubuntu_oci
+    boot_volume_size_in_gbs = "80"
+  }
+
+  metadata = {
+    ssh_authorized_keys = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPUTE_KEY_PUBLIC
+  }
+
 }
