b31dd9cf28230b90fd313a1c0a74f4c95150d2fd
Mervin Hemaraju <115166315+mervinhemaraju@users.noreply.github.com>
2024-02-17 19:25:58 +0400

    - Added inbound traffic 8050 for web-01 (#2)
    * - Added inbound traffic 8050 for web-01
    - Removed web-02 resources
    
    * - Removed unused outputs
    
    * - Removed unused datasources

--- compute.tf
+++ compute.tf
@@ -30,29 +30,29 @@ resource "oci_core_instance" "web_01" {
 }
 
 # * Create a compute instance for web 02
-resource "oci_core_instance" "web_02" {
+# resource "oci_core_instance" "web_02" {
 
-  availability_domain = data.oci_identity_availability_domain.availability_domain.name
-  compartment_id      = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
+#   availability_domain = data.oci_identity_availability_domain.availability_domain.name
+#   compartment_id      = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
 
-  display_name = "web-02"
+#   display_name = "web-02"
 
-  shape = "VM.Standard.E2.1.Micro"
+#   shape = "VM.Standard.E2.1.Micro"
 
-  create_vnic_details {
-    subnet_id        = oci_core_subnet.web_public.id
-    assign_public_ip = false
-    private_ip       = local.constants.compute.ip_address.web_02.private
-  }
+#   create_vnic_details {
+#     subnet_id        = oci_core_subnet.web_public.id
+#     assign_public_ip = false
+#     private_ip       = local.constants.compute.ip_address.web_02.private
+#   }
 
-  source_details {
-    source_type             = "image"
-    source_id               = local.constants.compute.image.ubuntu_oci
-    boot_volume_size_in_gbs = "80"
-  }
+#   source_details {
+#     source_type             = "image"
+#     source_id               = local.constants.compute.image.ubuntu_oci
+#     boot_volume_size_in_gbs = "80"
+#   }
 
-  metadata = {
-    ssh_authorized_keys = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPUTE_KEY_PUBLIC
-  }
+#   metadata = {
+#     ssh_authorized_keys = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPUTE_KEY_PUBLIC
+#   }
 
-}
+# }
