d1864c5c54716d9f37737df327959cecb1508321
Mervin Hemaraju <115166315+mervinhemaraju@users.noreply.github.com>
2024-04-08 11:49:44 +0400

    - Updated outputs (#8)

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+
+# Output the public IP for reserved public ip database
+output "database_mongo_reserved_ip" {
+  value = oci_core_public_ip.database.ip_address
+}
+
+# Output the private IP for mongo
+output "mongo_private_ip" {
+  value = oci_core_instance.mongo.private_ip
+}
