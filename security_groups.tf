0215f8e6819318869e26f976fda36b7fd66c1246
Mervin Hemaraju <115166315+mervinhemaraju@users.noreply.github.com>
2024-04-08 16:57:35 +0400

    - Enabled HTTP, HTTPS and mongodb traffic in security list (#9)

--- security_groups.tf
+++ security_groups.tf
@@ -53,5 +53,47 @@ resource "oci_core_security_list" "public_database" {
     description = "Allow all traffic for the vcn's cidr block."
   }
 
+  ingress_security_rules {
+
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    protocol    = 6 # TCP
+
+    tcp_options {
+      min = 80
+      max = 80
+    }
+
+    description = "Allow HTTP traffic"
+  }
+
+  ingress_security_rules {
+
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    protocol    = 6 # TCP
+
+    tcp_options {
+      min = 443
+      max = 443
+    }
+
+    description = "Allow HTTPS traffic"
+  }
+
+  ingress_security_rules {
+
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    protocol    = 6 # TCP
+
+    tcp_options {
+      min = 27017
+      max = 27017
+    }
+
+    description = "Allow MongoDB traffic"
+  }
+
   freeform_tags = local.tags.defaults
 }
