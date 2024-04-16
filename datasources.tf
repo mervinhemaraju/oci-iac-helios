a494bdf6e9d8a5c740d6e65d0ba8f4a63cfc2305
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-16 14:18:55 +0400

    - Added fixed image id on compute instance to prevent a rebuild

--- datasources.tf
+++ datasources.tf
@@ -2,12 +2,12 @@
 data "doppler_secrets" "prod_main" {}
 
 # Get the latest Oracle 9 Image
-data "oci_core_images" "oracle_linux" {
-  compartment_id           = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
-  operating_system         = "Oracle Linux"
-  operating_system_version = "9"
-  shape                    = local.values.compute.shape
-}
+# data "oci_core_images" "oracle_linux" {
+#   compartment_id           = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
+#   operating_system         = "Oracle Linux"
+#   operating_system_version = "9"
+#   shape                    = local.values.compute.shape
+# }
 
 # Gets the availability domain from OCI
 data "oci_identity_availability_domain" "this" {
