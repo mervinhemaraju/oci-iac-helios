206c34c839f9d10cb3a2443e68ee77458bc5be1c
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-05-01 14:11:35 +0400

    -

--- datasources.tf
+++ datasources.tf
@@ -3,23 +3,23 @@ data "doppler_secrets" "prod_main" {}
 
 # Gets the availability domain from OCI
 data "oci_identity_availability_domain" "this" {
-  compartment_id = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
+  compartment_id = local.values.compartments.production
   ad_number      = 1
 }
 
 # Gets the fault domains from this AZ
 data "oci_identity_fault_domains" "this" {
+  compartment_id      = local.values.compartments.production
   availability_domain = data.oci_identity_availability_domain.this.name
-  compartment_id      = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
 }
 
-# Get ths private ips for mongo compute
-data "oci_core_private_ips" "mongo" {
-  ip_address = local.networking.ip_address.mongo
-  subnet_id  = oci_core_subnet.public_database.id
+# # Get ths private ips for mongo compute
+# data "oci_core_private_ips" "mongo" {
+#   ip_address = local.networking.ip_address.mongo
+#   subnet_id  = oci_core_subnet.public_database.id
 
-  depends_on = [
-    oci_core_instance.mongo,
-    oci_core_subnet.public_database
-  ]
-}
+#   depends_on = [
+#     oci_core_instance.mongo,
+#     oci_core_subnet.public_database
+#   ]
+# }
