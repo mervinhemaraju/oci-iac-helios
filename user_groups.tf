be3182ab223aa0e81b332493d39c36e00e75b243
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-05-01 15:37:00 +0400

    - Fixed user group ocid

--- user_groups.tf
+++ user_groups.tf
@@ -1,7 +1,7 @@
 # Creates a new user gorup for drg admins
 resource "oci_identity_group" "drg_admins" {
 
-  compartment_id = local.values.compartments.production
+  compartment_id = local.values.tenancy
 
   description = "Dynamic Routing Gateway Admininistrators"
   name        = "drg-admins"
