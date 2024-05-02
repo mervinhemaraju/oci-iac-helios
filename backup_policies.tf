4d7a92e9513a4c26476d9fe52a978f229aaacf5b
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-05-02 21:10:14 +0400

    - Added volume backups and backup policies

--- backup_policies.tf
+++ backup_policies.tf
@@ -1,28 +1,28 @@
-# resource "oci_core_volume_backup_policy" "weekly" {
-#   compartment_id = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
+resource "oci_core_volume_backup_policy" "weekly" {
+  compartment_id = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
 
-#   display_name  = "weekly-backups"
-#   freeform_tags = local.tags.defaults
+  display_name  = "weekly-backups"
+  freeform_tags = local.tags.defaults
 
-#   schedules {
+  schedules {
 
-#     backup_type       = "FULL"
-#     period            = "ONE_WEEK"
-#     retention_seconds = 691200 # 8 days
+    backup_type       = "FULL"
+    period            = "ONE_WEEK"
+    retention_seconds = 691200 # 8 days
 
-#     day_of_week = "SATURDAY"
-#     hour_of_day = 1
-#     time_zone   = "UTC"
-#   }
-# }
+    day_of_week = "SATURDAY"
+    hour_of_day = 1
+    time_zone   = "UTC"
+  }
+}
 
-# resource "oci_core_volume_group_backup" "database" {
+resource "oci_core_volume_group_backup" "database" {
 
-#   volume_group_id = oci_core_volume_group.database.id
+  volume_group_id = oci_core_volume_group.database.id
 
-#   compartment_id = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
+  compartment_id = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
 
-#   display_name  = "database-volumes-backup"
-#   freeform_tags = local.tags.defaults
-#   type          = "FULL"
-# }
+  display_name  = "database-volumes-backup"
+  freeform_tags = local.tags.defaults
+  type          = "FULL"
+}
--- volume_groups.tf
+++ volume_groups.tf
@@ -1,14 +1,14 @@
-# resource "oci_core_volume_group" "database" {
-#   availability_domain = data.oci_identity_availability_domain.this.name
-#   compartment_id      = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
+resource "oci_core_volume_group" "database" {
+  availability_domain = data.oci_identity_availability_domain.this.name
+  compartment_id      = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
 
-#   display_name = "database-volumes"
+  display_name = "database-volumes"
 
-#   source_details {
-#     type = "volumeIds"
-#     volume_ids = [
-#       oci_core_instance.mongo.boot_volume_id
-#     ]
-#   }
-#   freeform_tags = local.tags.defaults
-# }
+  source_details {
+    type = "volumeIds"
+    volume_ids = [
+      oci_core_instance.mongo.boot_volume_id
+    ]
+  }
+  freeform_tags = local.tags.defaults
+}
