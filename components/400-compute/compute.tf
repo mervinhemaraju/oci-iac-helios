65ebbfd843c767527579954cbd570891687ff388
mervin.hemaraju <mervinhemaraju16@gmail.com>
2024-12-27 15:11:09 +0400

    - Updated compute

--- components/400-compute/compute.tf
+++ components/400-compute/compute.tf
@@ -13,8 +13,8 @@ resource "oci_core_instance" "web_01" {
 
   shape_config {
     memory_in_gbs = 12
-    ocpus         = 2
-    vcpus         = 2
+    ocpus         = 4
+    vcpus         = 4
   }
 
   create_vnic_details {
@@ -62,8 +62,8 @@ resource "oci_core_instance" "web_02" {
 
   shape_config {
     memory_in_gbs = 12
-    ocpus         = 2
-    vcpus         = 2
+    ocpus         = 4
+    vcpus         = 4
   }
 
   create_vnic_details {
