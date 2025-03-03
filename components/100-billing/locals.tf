b7771881311b3bc0da869ec69133884e844b848f
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-03-04 00:05:10 +0400

    - Added missing locals

--- components/100-billing/locals.tf
+++ components/100-billing/locals.tf
@@ -11,4 +11,12 @@ locals {
       "Component"   = "100-billing"
     }
   }
+
+  values = {
+    # tenancy = data.doppler_secrets.prod_main.map.OCI_HELIOS_TENANCY_OCID
+    compartments = {
+      production = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
+      root       = data.doppler_secrets.prod_main.map.OCI_HELIOS_COMPARTMENT_ROOT_ID
+    }
+  }
 }
