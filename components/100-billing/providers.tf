ad441aa3c5434d8e3274e6aa9665c58e4e38a8b8
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-03-04 01:13:03 +0400

    - Fixed invalid referecne in providers

--- components/100-billing/providers.tf
+++ components/100-billing/providers.tf
@@ -22,7 +22,7 @@ terraform {
   # Backend configuration
   backend "s3" {
     region = var.bucket_region
-    key    = "${bucket_key_prefix_iac}/billing/state.tf"
+    key    = "${var.bucket_key_prefix_iac}/billing/state.tf"
     bucket = var.bucket_name
   }
 
