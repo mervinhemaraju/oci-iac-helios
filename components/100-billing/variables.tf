31773314a4919974a06b638353a681e6c86ed3b5
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-03-04 00:59:54 +0400

    -

--- components/100-billing/variables.tf
+++ components/100-billing/variables.tf
@@ -9,7 +9,7 @@ variable "token_doppler_iac_cloud_main" {
   description = "The Doppler token for the secrets manager cloud main repo."
 }
 
-variable "bucket_key_prefix_iac" {
+variable "bucket_key_iac" {
   type        = string
   description = "The prefix for the bucket key."
 }
