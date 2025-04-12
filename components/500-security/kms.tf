353ed2e3cf611fcdfab4c135edc43b233890ff40
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-04-12 19:34:33 +0400

    - Added hsm key to vault

--- components/500-security/kms.tf
+++ components/500-security/kms.tf
@@ -1,12 +1,12 @@
-# resource "oci_kms_key" "mervinhemaraju_root_ca" {
-#   compartment_id = local.values.compartments.production
-#   display_name   = "mervinhemaraju-root-ca-key"
-#   key_shape {
-#     algorithm = "RSA"
-#     length    = "2048"
-#   }
-#   management_endpoint = "CREATE"
-#   protection_mode     = "HSM"
+resource "oci_kms_key" "mervinhemaraju_root_ca" {
+  compartment_id      = local.values.compartments.production
+  display_name        = "mervinhemaraju-root-ca-key"
+  management_endpoint = oci_kms_vault.mervinhemaraju_root_ca.management_endpoint
+  key_shape {
+    algorithm = "RSA"
+    length    = "2048"
+  }
+  protection_mode = "HSM"
 
-#   freeform_tags = local.tags.defaults
-# }
+  freeform_tags = local.tags.defaults
+}
