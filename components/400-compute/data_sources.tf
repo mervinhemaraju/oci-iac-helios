d10203173348ac7f71ea74fc6bea37c08fd82970
Mervin Hemaraju <th3pl4gu33@gmail.com>
2026-01-21 20:15:54 +0400

    - Added new oke cluster and bastions

---
+++ components/400-compute/data_sources.tf
@@ -0,0 +1,51 @@
+# Define our data source to fetch secrets
+data "doppler_secrets" "oci_creds" {
+  project = "cloud-oci-creds"
+}
+
+# Gets the availability domain from OCI
+data "oci_identity_availability_domain" "this" {
+  compartment_id = local.values.compartments.production
+  ad_number      = 1
+}
+
+data "oci_core_vcns" "dev" {
+  compartment_id = local.values.compartments.production
+
+  display_name = "dev"
+}
+
+data "oci_core_subnets" "public_k8" {
+  compartment_id = local.values.compartments.production
+  display_name   = "public-k8"
+  vcn_id         = data.oci_core_vcns.dev.virtual_networks[0].id
+}
+
+data "oci_core_subnets" "private_k8_api" {
+  compartment_id = local.values.compartments.production
+  display_name   = "private-k8-api"
+  vcn_id         = data.oci_core_vcns.dev.virtual_networks[0].id
+}
+
+data "oci_core_subnets" "private_k8" {
+  compartment_id = local.values.compartments.production
+  display_name   = "private-k8"
+  vcn_id         = data.oci_core_vcns.dev.virtual_networks[0].id
+}
+
+data "oci_core_subnets" "private_mgmt" {
+  compartment_id = local.values.compartments.production
+  display_name   = "private-mgmt"
+  vcn_id         = data.oci_core_vcns.dev.virtual_networks[0].id
+}
+
+data "oci_containerengine_node_pool_option" "arm_oke_ol" {
+  compartment_id = local.values.compartments.production
+
+  node_pool_option_id = "all"
+
+  # Filter for specific requirements
+  node_pool_k8s_version = "v1.34.1"
+  node_pool_os_arch     = "AARCH64"
+  node_pool_os_type     = "OL8"
+}
---
+++ components/400-compute/locals.tf
@@ -0,0 +1,58 @@
+
+locals {
+
+  tags = {
+    defaults = {
+      "Creator"     = "mervin.hemaraju",
+      "Owner"       = "mervin.hemaraju",
+      "Terraform"   = "Yes",
+      "Project"     = "https://github.com/mervinhemaraju/oci-iac-helios",
+      "Environment" = "Production"
+      "Component"   = "400-compute"
+    }
+  }
+
+  values = {
+
+    compartments = {
+      production = data.doppler_secrets.oci_creds.map.OCI_HELIOS_COMPARTMENT_PRODUCTION_ID
+    }
+
+
+    bastions = {
+      "private-jump-01" = {
+        max_session_ttl_in_seconds   = 10800
+        client_cidr_block_allow_list = ["0.0.0.0/0"]
+      }
+      "private-jump-02" = {
+        max_session_ttl_in_seconds   = 10800
+        client_cidr_block_allow_list = ["0.0.0.0/0"]
+      }
+    }
+
+    compute = {
+
+      shape = "VM.Standard.A1.Flex"
+
+      plugins_config = [
+        {
+          name          = "Bastion"
+          desired_state = "ENABLED"
+        },
+        {
+          name          = "OS Management Service Agent"
+          desired_state = "ENABLED"
+        },
+        {
+          name          = "Compute Instance Run Command"
+          desired_state = "ENABLED"
+        },
+        {
+          name          = "Compute Instance Monitoring"
+          desired_state = "ENABLED"
+        }
+      ]
+    }
+  }
+
+}
