8993129ee5baf20c389888acecf619c20f63d51b
Mervin Hemaraju <th3pl4gu33@gmail.com>
2026-01-21 23:07:53 +0400

    - Added drg admins in secrets

--- components/200-iam/secrets.tf
+++ components/200-iam/secrets.tf
@@ -6,7 +6,9 @@ resource "doppler_secret" "helios_groups" {
   value_type = "json"
   value = jsonencode(
     {
-      "${local.values.groups.vcn_admins}" : oci_identity_group.vcn_admins.id
+      "${local.values.groups.vcn_admins}" : oci_identity_group.vcn_admins.id,
+      "${local.values.groups.drg_admins}" : oci_identity_group.drg_admins.id
     }
   )
 }
+
